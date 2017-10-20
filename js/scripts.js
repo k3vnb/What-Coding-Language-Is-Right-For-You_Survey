f010d6a59be79da65048781c2c9113e06a8b447e
Kevin Boyle <papershack@gmail.com>
2017-10-20 10:02:45 -0700

    add doc ready function to js, set .submit function, declare variables

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,16 @@
 $(document).ready(function() {
+  $("form#codequiz").submit(function(event) {
+    var q1 = $("select#q1").val();
+    var q2 = $("select#q2").val();
+    var q3 = $("select#q3").val();
+    var q4 = $("select#q4").val();
+    var q5 = $("select#q5").val();
+
+    if (q3 === "hate") {
+      alert("what?");
+    } else if (q1 === )
+    alert("Test");
+  });
+
 
 });
