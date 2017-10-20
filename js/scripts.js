d735818adc3cfbc2dc5702986f82ca575d15386e
Kevin Boyle <papershack@gmail.com>
2017-10-20 13:46:53 -0700

    add .hide function to each branch statement

--- js/scripts.js
+++ js/scripts.js
@@ -9,22 +9,30 @@ $(document).ready(function() {
     if (q1, q2, q3, q4, q5 === "none") {
       alert("Please complete the form");
     } else if (q3 === "hate") {
+      $("#phpnet, #design, #rubyreact, #designruby, #javaphp").hide();
       $("#other").show();
     } else if (q1 === "corp" && q2 === "back") {
+      $("#other, #design, #rubyreact, #designruby, #javaphp").hide();
       $("#phpnet").show();
     }  else if (q1 === "corp" && q2 === "front") {
+      $("#phpnet, #other, #rubyreact, #designruby, #javaphp").hide();
       $("#design").show();
-    } else if (q1 !== "corp" && q2 === "front" || q1 !== "corp" && q2 === "back") {
+    } else if (q1 === "smbus" && q2 === "front" || q1 !== "corp" && q2 === "back") {
+      $("#phpnet, #design, #other, #designruby, #javaphp").hide();
       $("#rubyreact").show();
     } else if (q1 === "ind" && q2 === "front") {
+      $("#phpnet, #design, #rubyreact, #other, #javaphp").hide();
       $("#designruby").show();
     } else if (q1 === "ind" && q2 === "back") {
+      $("#phpnet, #design, #rubyreact, #designruby, #other").hide();
       $("#javaphp").show();
     };
 
     if (q5 === "big") {
+      $("#lemur").hide();
       $("#minotaur").show();
     } else if (q5 === "small") {
+      $("#minotaur").hide();
       $("#lemur").show();
     };
     alert("Test");
