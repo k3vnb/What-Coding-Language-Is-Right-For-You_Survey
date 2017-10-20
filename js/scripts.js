0310e669d8140c8e8a3b68b80b7be335976b1df2
Kevin Boyle <papershack@gmail.com>
2017-10-20 11:37:02 -0700

    create new divs for results of survey, add id tags to divs

--- js/scripts.js
+++ js/scripts.js
@@ -7,20 +7,20 @@ $(document).ready(function() {
     var q5 = $("select#q5").val();
 
     if (q3 === "hate") {
-      alert("what?");
+      $("#other")
     } else if (q1, q2, q3, q4, q5 === "none") {
       alert("Please complete the form");
     } else if (q1 === "corp" && q2 === "back") {
-      alert("PHP, C#, or Java");
+      alert("PHP/Drupal or C#/.Net");
     } else if (q1 !== "corp" && q2 === "front" || q1 !== "corp" && q2 === "back") {
-      alert("Ruby or React");
+      alert("Ruby/Rails or React");
     } else if (q1 === "ind" && q2 === "front") {
-      alert("Ruby, React or Design");
+      alert("Ruby or CSS/Design");
     } else if (q1 === "ind" && q2 === "back") {
-      alert("PHP, C#, or Java")
+      alert("PHP/Drupal or Java/Android")
     }
     alert("Test");
-  
+
     event.preventDefault();
   });
 });
